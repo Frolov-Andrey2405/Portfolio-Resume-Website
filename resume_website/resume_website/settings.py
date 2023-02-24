b7601aa94632f9fa0862ec4a19c342e18c46de12
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-02-24 17:02:15 +0200

    Static files, page styling with Bootstrap

--- resume_website/resume_website/settings.py
+++ resume_website/resume_website/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5t@=g-w(0cv(ui_-b7hnu@9^^4fyr2s&z@a6g=gaheaiwrv921'
+SECRET_KEY = '''
+django-insecure-5t@=g-w(0cv(ui_-b7hnu@9^^4fyr2s&z@a6g=gaheaiwrv921'''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
